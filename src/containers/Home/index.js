1ac934042d63c916fa373bbd918e4becd88d9166
Nilesh Chavan <nilesh.chavan@neosoftmail.com>
2022-03-13 13:39:44 +0530

    Home Design

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image, ScrollView} from 'react-native';
 import {ADS} from '../../components/Ads';
 import LocationHeader from '../../components/LocationHeader';
 import {CategoryCard} from '../../components/CategoryCard';
+import {SaleCard} from '../../components/SaleCard';
+import Carousel from 'react-native-snap-carousel';
+import {CustomText} from '../../components/Text';
+import {TopPicCard} from './TopPicCard/index';
+import Icon from 'react-native-vector-icons/Fontisto';
 
 function Home() {
   const data = [
@@ -47,48 +52,142 @@ function Home() {
       title: 'Speciality Stores',
     },
   ];
+
+  const offerData = [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2epi8rpNSNSSUCd4Lz-BXs7oK8tCgnWoxCQ&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUa7O_OGQ-gzkFhllibzg1EYMQFj9ZMpLS1A&usqp=CAU',
+  ];
+
+  const topPics = [
+    {
+      imageURI:
+        'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wfjvzbsyjruydaozbnzo',
+      off: '90%',
+      upTo: '120',
+      name: 'Red Hot Chillies',
+      time: '29 mins',
+    },
+    {
+      imageURI:
+        'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wfjvzbsyjruydaozbnzo',
+      off: '50%',
+      upTo: '110',
+      name: 'Fried Nation',
+      time: '32 mins',
+    },
+    {
+      imageURI:
+        'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wfjvzbsyjruydaozbnzo',
+      // off:'10%',
+      // upTo:'10',
+      name: 'The Thali',
+      time: '34 mins',
+    },
+    {
+      imageURI:
+        'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wfjvzbsyjruydaozbnzo',
+      off: '90%',
+      upTo: '120',
+      name: 'Red Hot Chillies',
+      time: '29 mins',
+    },
+    {
+      imageURI:
+        'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wfjvzbsyjruydaozbnzo',
+      // off:'10%',
+      // upTo:'10',
+      name: 'The Thali',
+      time: '34 mins',
+    },
+    {
+      imageURI:
+        'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wfjvzbsyjruydaozbnzo',
+      off: '90%',
+      upTo: '120',
+      name: 'Red Hot Chillies',
+      time: '29 mins',
+    },
+    {
+      imageURI:
+        'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wfjvzbsyjruydaozbnzo',
+      // off:'10%',
+      // upTo:'10',
+      name: 'The Thali',
+      time: '34 mins',
+    },
+    {
+      imageURI:
+        'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wfjvzbsyjruydaozbnzo',
+      off: '90%',
+      upTo: '120',
+      name: 'Red Hot Chillies',
+      time: '29 mins',
+    },
+  ];
   return (
     <View
       style={{
         flex: 1,
         backgroundColor: 'white',
       }}>
-      <View style={{paddingHorizontal: 15}}>
-        <LocationHeader />
-        <View style={{marginTop: 10}}>
-          <ADS />
+      <ScrollView>
+        <View style={{paddingHorizontal: 15}}>
+          <LocationHeader />
+          <View style={{marginTop: 10}}>
+            <ADS />
+          </View>
         </View>
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          justifyContent: 'space-evenly',
-          marginTop: 20,
-        }}>
-        {data.map((i, index) => {
-          if (data.length / 2 > index) {
-            console.log(index);
-            return (
-              <View>
-                <CategoryCard
-                  title={data[2 * index].title}
-                  imgSrc={data[2 * index].imgSrc}
-                />
-                {data?.[2 * index + 1]?.title != undefined && (
+        <View style={{alignItems: 'center'}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+              marginTop: 20,
+            }}>
+            {data.map((i, index) => {
+              return (
+                <View key={index}>
                   <CategoryCard
-                    title={data[2 * index + 1].title}
-                    imgSrc={data[2 * index + 1].imgSrc}
+                    title={data[index].title}
+                    imgSrc={data[index].imgSrc}
                   />
-                )}
-              </View>
-            );
-          } else {
-            return <></>;
-          }
-        })}
-      </View>
-      {/* <Text style={{color: 'black'}}>Home</Text> */}
+                </View>
+              );
+            })}
+          </View>
+        </View>
+        <View
+          style={{
+            paddingHorizontal: 15,
+            width: '100%',
+            marginBottom: 30,
+            marginTop: 20,
+          }}>
+          <Image
+            style={{width: '100%', height: 200, borderRadius: 10}}
+            source={{
+              uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2epi8rpNSNSSUCd4Lz-BXs7oK8tCgnWoxCQ&usqp=CAU',
+            }}
+          />
+        </View>
+        <View
+          style={{flexDirection: 'row', paddingLeft: 15, alignItems: 'center'}}>
+          <Icon name="like" size={20} color="black" />
+
+          <CustomText style={{marginLeft: 8, fontWeight: '700'}}>
+            Top Picks For You
+          </CustomText>
+        </View>
+
+        <ScrollView
+          horizontal={true}
+          style={{paddingLeft: 15, marginTop: 18, marginBottom: 30}}>
+          {topPics.map((prod, index) => {
+            return <TopPicCard key={index} {...prod} />;
+          })}
+        </ScrollView>
+      </ScrollView>
     </View>
   );
 }
