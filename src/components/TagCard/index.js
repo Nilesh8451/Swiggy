29f37604d99afd5544ddffe988be8aa8148edc24
Nilesh Chavan <nilesh.chavan@neosoftmail.com>
2022-03-13 16:09:45 +0530

    Search Design

---
+++ src/components/TagCard/index.js
@@ -0,0 +1,36 @@
+import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from 'react';
+import {CustomText} from '../Text';
+
+const TagCard = props => {
+  return (
+    <View style={{maxWidth: 140, height: 40, marginBottom: 10}}>
+      <View
+        style={{
+          paddingHorizontal: 10,
+          flexDirection: 'row',
+          alignItems: 'center',
+          // backgroundColor: 'red',
+          borderWidth: 0.4,
+          borderRadius: 20,
+          borderColor: 'gray',
+          marginRight: 10,
+
+          overflow: 'hidden',
+          height: '100%',
+        }}>
+        <Icon name="history" size={20} color="gray" style={{opacity: 0.5}} />
+        <View>
+          <CustomText
+            numberOfLines={1}
+            style={{paddingLeft: 7, fontSize: 10, opacity: 0.7}}>
+            {props.name}
+          </CustomText>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default TagCard;
